c58022f45545a58ffbbb47c0a82a230482539993
edwindvd <edwindavidcr7@gmail.com>
2022-07-05 18:28:47 +0000

    ultimo

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -3,7 +3,7 @@ import {PropTypes} from "prop-types";
 
 export const Counter = (props) => {
 return (
-    <div className="Card container">
+    <div className="Card">
     <div className="number">
         <h1>{props.unit}</h1>
     </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import {Counter} from "./counter.jsx";
 import {PropTypes} from "prop-types"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 //create your first component
 const Home = (props) => {
 	return (
-		<div className="row">
-			<div className="col-2"> <Counter unit={Math.floor(props.seconds/10000)%10}/> </div>
-			<div className="col-2"> <Counter unit={Math.floor(props.seconds/10000)%10}/> </div>
-			<div className="col-2"> <Counter unit={Math.floor(props.seconds/1000)%10}/> </div>
-			<div className="col-2"> <Counter unit={Math.floor(props.seconds/100)%10}/> </div>
-			<div className="col-2"> <Counter unit={Math.floor(props.seconds/10)%10}/> </div>
-			<div className="col-2"> <Counter unit={Math.floor(props.seconds/1)%10}/> </div>		
+		<div className="d-flex justify-content-center">
+			<div className="display px-2 fs-1 bg-dark text-light border border-light rounded"> <FontAwesomeIcon icon={faClock} /> </div>
+			<div className="display-inline px-3 bg-dark text-light border border-dark rounded"> <Counter unit={Math.floor(props.seconds/10000)%10}/> </div>
+			<div className="display-inline px-3 bg-dark text-light border border-dark rounded"> <Counter unit={Math.floor(props.seconds/10000)%10}/> </div>
+			<div className="display-inline px-3 bg-dark text-light border border-dark rounded"> <Counter unit={Math.floor(props.seconds/1000)%10}/> </div>
+			<div className="display-inline px-3 bg-dark text-light border border-dark rounded"> <Counter unit={Math.floor(props.seconds/100)%10}/> </div>
+			<div className="display-inline px-3 bg-dark text-light border border-dark rounded"> <Counter unit={Math.floor(props.seconds/10)%10}/> </div>
+			<div className="display-inline px-3 bg-dark text-light border border-dark rounded"> <Counter unit={Math.floor(props.seconds/1)%10}/> </div>		
 		</div>
 	);
 };
